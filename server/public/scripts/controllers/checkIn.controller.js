fc193c912208f66bbf2a215ae420b51be2fa05eb
Samantha Martorana <samanthamartorana7@gmail.com>
2017-10-18 17:29:14 -0500

    completed the quiz

--- server/public/scripts/controllers/checkIn.controller.js
+++ server/public/scripts/controllers/checkIn.controller.js
@@ -1,80 +1,101 @@
 myApp.controller('CheckInController', function (TrackerService, $mdDialog) {
-        var vm = this;
+    var vm = this;
+    vm.flow = '';
+    vm.mood = '',
 
         vm.status = ' ';
-        vm.items = [1, 2, 3, 4, 5];
+    vm.items = [1, 2, 3, 4, 5];
 
-        console.log('CheckInController created');
+    console.log('CheckInController created');
 
-        vm.TrackerService = TrackerService;
+    vm.TrackerService = TrackerService;
 
-        // vm.showAdvanced = function(event) {
-        //     $mdDialog.show ({
-        //        clickOutsideToClose: true,
-        //        scope: vm,        
-        //        preserveScope: true,           
-        //        templateURL: '/views/templates/quiz.html'
-        //        controller: function DialogController($mdDialog) {
-        //            var vm = this;
-        //            vm.closeDialog = function() {
-        //              $mdDialog.hide();
-        //           }
-        //        }
-        //     });
-        //  };          
-        vm.user = {
-            title: 'Developer',
-            email: 'ipsum@lorem.com',
-            firstName: '',
-            lastName: '',
-            company: 'Google',
-            address: '1600 Amphitheatre Pkwy',
-            city: 'Mountain View',
-            state: 'CA',
-            biography: 'Loves kittens, snowboarding, and can type at 130 WPM.\n\nAnd rumor has it she bouldered up Castle Craig!',
-            postalCode: '94043'
-        };
-    //     vm.symptoms = [{
-    //         name: chills
-    //     }, {
-    //       name: irritable
-    //   }];
-        vm.data = {
-          flow : 'light',
-          hasPeriod : true,
-          date : new Date(),
-          mood: 'happy',
-        };
-        
-            // Lists of fruit names and Vegetable objects
-          vm.message = 'false';
+    vm.data = {
+        flow: 'light',
+        hasPeriod: 'no',
+        date: new Date(),
+    };
 
-        
-          vm.onChange = function(hasPeriodState) {
-              vm.message = hasPeriodState;
-          };
+    vm.symptoms = []
 
-          vm.onChange = function(flowState) {
-              vm.message = flowState;
-          }
-          
-          
+    if (vm.data.hasPeriod = 'no') {
+        vm.flow = 'none';
+    };
 
-        vm.states = ('AL AK AZ AR CA CO CT DE FL GA HI ID IL IN IA KS KY LA ME MD MA MI MN MS ' +
-            'MO MT NE NV NH NJ NM NY NC ND OH OK OR PA RI SC SD TN TX UT VT VA WA WV WI ' +
-            'WY').split(' ').map(function (state) {
-            return {
-                abbrev: state
-            };
-        });
-    })
-        
-    .config(function ($mdThemingProvider) {
 
-        // Configure a dark theme with primary foreground yellow
+// Lists of fruit names and Vegetable objects
 
-        $mdThemingProvider.theme('docs-dark', 'default')
-            .primaryPalette('yellow')
-            .dark();
 
-    });
+// vm.onChange = function (hasPeriodState) {
+//     vm.message = hasPeriodState;
+//     vm.flow = 'none';
+//     console.log('flow', vm.flow)
+// };
+
+vm.changeFlow = function() {
+    vm.flow = 'none';
+}
+
+vm.toggleHeavy = function () {
+    vm.heavy = true
+    console.log(vm.heavy);
+    if (vm.heavy) {
+        vm.flow = 'heavy';
+    }
+}
+vm.toggleMedium = function () {
+    vm.medium = true
+    console.log(vm.medium);
+    if (vm.heavy) {
+        vm.flow = 'medium';
+    }
+}
+
+vm.toggleLight = function () {
+    vm.light = true
+    console.log(vm.light);
+    if (vm.light) {
+        vm.flow = 'light';
+    }
+}
+
+vm.toggleVeryBad = function () {
+    vm.veryBad = true
+    console.log(vm.veryBad);
+    if (vm.veryBad) {
+        vm.mood = 'very bad';
+    }
+}
+
+vm.toggleBad = function () {
+    vm.bad = true
+    console.log(vm.bad);
+    if (vm.bad) {
+        vm.mood = 'bad';
+    }
+}
+
+vm.toggleNeutral = function () {
+    vm.neutral = true
+    console.log(vm.neutral);
+    if (vm.neutral) {
+        vm.mood = 'neutral';
+    }
+}
+
+vm.toggleGood = function () {
+    vm.good = true
+    console.log(vm.good);
+    if (vm.good) {
+        vm.mood = 'good';
+    }
+}
+
+vm.toggleVeryGood = function () {
+    vm.veryGood = true
+    console.log(vm.veryGood);
+    if (vm.veryGood) {
+        vm.mood = 'very good';
+    }
+}
+});
